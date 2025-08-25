5766e8d97852a5875c230d830b8e932852008815
Spi3lot <spi3lot@gmail.com>
2025-08-26 00:01:46 +0200

    Make audio downloading work when double clicking video result

--- scripts/Mosic.cs
+++ scripts/Mosic.cs
@@ -1,21 +1,30 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Godot;
+using YoutubeSearchApi.Net.Models.Youtube;
 
 namespace Mosic.scripts;
 
 public partial class Mosic : Control
 {
-    private readonly IProgress<YoutubeDLSharp.DownloadProgress> _progress;
+    private readonly YoutubeSearchApi.Net.Services.YoutubeSearchClient _youtubeSearchClient;
 
     private readonly System.Net.Http.HttpClient _httpClient = new();
 
-    private readonly YoutubeSearchApi.Net.Services.YoutubeSearchClient _youtubeSearchClient;
+    private readonly IList<YoutubeVideo> _videos = [];
+
+    private readonly YoutubeDLSharp.YoutubeDL _ytdl = new();
+
+    private readonly IProgress<YoutubeDLSharp.DownloadProgress> _progress;
 
     public Mosic()
     {
         _progress = new Progress<YoutubeDLSharp.DownloadProgress>(progress =>
-            DownloadProgressBar.Value = progress.Progress);
+        {
+            DownloadProgressBar.Value = progress.Progress;
+        });
+
         _youtubeSearchClient = new(_httpClient);
     }
 
@@ -28,49 +37,47 @@ public partial class Mosic : Control
     public override void _Ready()
     {
         SearchBar.GrabFocus();
-        
-        SearchBar.Connect(
-            LineEdit.SignalName.TextSubmitted,
-            Callable.From(void (string query) => Search(query))
-        );
+        SearchBar.Connect(LineEdit.SignalName.TextSubmitted, Callable.From(void (string query) => Search(query)));
+        SearchBar.PlaceholderText = Tr("SEARCH");
+
+        SearchResultList.FixedIconSize = DisplayServer.ScreenGetSize() / 10;
+        SearchResultList.Connect(ItemList.SignalName.ItemActivated, Callable.From(void (int index) => Download(index)));
     }
 
-    private void Download(string url)
+    private async Task Download(int index)
     {
-        var ytdl = new YoutubeDLSharp.YoutubeDL();
-        ytdl.RunAudioDownload(url, progress: _progress);
+        DownloadProgressBar.Indeterminate = true;
+        await _ytdl.RunAudioDownload(_videos[index].Url, progress: _progress);
+        DownloadProgressBar.Indeterminate = false;
     }
 
     private async Task Search(string query)
     {
+        if (string.IsNullOrWhiteSpace(query))
+        {
+            return;
+        }
+
+        _videos.Clear();
         SearchResultList.Clear();
         DownloadProgressBar.Indeterminate = true;
         var result = await _youtubeSearchClient.SearchAsync(query);
 
         foreach (var video in result.Results)
         {
-            GD.Print(video.ThumbnailUrl);
-            byte[] bytes = await _httpClient.GetByteArrayAsync(video.ThumbnailUrl);
+            if (video.ThumbnailUrl.Contains("png", StringComparison.OrdinalIgnoreCase))
+            {
+                GD.PushWarning(video.ThumbnailUrl + " is probably png");
+            }
+
             var image = new Image();
-            image.LoadJpgFromBuffer(bytes);
-            ResizeImageKeepAspect(image, 100, false);
+            image.LoadJpgFromBuffer(await _httpClient.GetByteArrayAsync(video.ThumbnailUrl));
 
-            string text = $"{video.Title}\n{video.Author}\n{video.Duration}";
+            string text = $"{video.Title} | {video.Author} | {video.Duration}";
             SearchResultList.AddItem(text, ImageTexture.CreateFromImage(image));
+            _videos.Add(video);
         }
-        
-        DownloadProgressBar.Indeterminate = false;
-    }
 
-    private static void ResizeImageKeepAspect(Image image, int size, bool setWidth)
-    {
-        if (setWidth)
-        {
-            image.Resize(size, size * image.GetHeight() / image.GetWidth());
-        }
-        else
-        {
-            image.Resize(size * image.GetWidth() / image.GetHeight(), size);
-        }
+        DownloadProgressBar.Indeterminate = false;
     }
 }
