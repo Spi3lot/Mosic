292c57357724c527d0d2423fa94d570c75193ef2
Spi3lot <spi3lot@gmail.com>
2025-09-23 00:47:02 +0200

    Implement installer that supports .zip, .tar(.gz) and just raw .exe of course

---
+++ scripts/Service/ArchiveInstaller.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Formats.Tar;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Godot;
+
+namespace Mosic.Scripts.Service;
+
+public class ArchiveInstaller : IInstaller
+{
+    public async Task<string> InstallAsync(string path, byte[] bytes)
+    {
+        string extension = Path.GetExtension(path).ToLowerInvariant();
+
+        switch (extension)
+        {
+            case ".exe":
+                await File.WriteAllBytesAsync(path, bytes);
+                return path;
+            case ".zip":
+                await File.WriteAllBytesAsync(path, bytes);
+                return await InstallZipAsync(path);
+            case ".gz" when path.EndsWith(".tar.gz"):
+            case ".tar" or ".tgz":
+                return await InstallTarAsync(path, bytes, gzip: extension.EndsWith("gz"));
+            default:
+                GD.PushError($"Unknown update format: {extension}");
+                return null;
+        }
+    }
+
+    private static async Task<string> InstallZipAsync(string path)
+    {
+        string directory = Path.GetDirectoryName(path)!;
+        using var reader = new ZipReader();
+        var error = reader.Open(path);
+
+        if (error != Error.Ok)
+        {
+            GD.PushError($"Error opening ZipReader for '{path}': {error}");
+            return null;
+        }
+
+        string executablePath = null;
+        var tasks = new List<Task>();
+
+        foreach (string file in reader.GetFiles())
+        {
+            string destinationPath = Path.Combine(directory, file);
+            byte[] bytes = reader.ReadFile(file);
+            var task = File.WriteAllBytesAsync(destinationPath, bytes);
+            tasks.Add(task);
+
+            if (IsExecutable(bytes))
+            {
+                executablePath = destinationPath;
+            }
+        }
+
+        await Task.WhenAll(tasks);
+        return executablePath;
+    }
+
+    private static async Task<string> InstallTarAsync(string path, byte[] bytes, bool gzip)
+    {
+        Stream stream = new MemoryStream(bytes);
+
+        if (gzip)
+        {
+            stream = new GZipStream(stream, CompressionMode.Decompress);
+        }
+
+        string directory = Path.GetDirectoryName(path)!;
+        string executablePath = null;
+        await using var reader = new TarReader(stream);
+
+        while (await reader.GetNextEntryAsync() is { } entry)
+        {
+            if (entry.Length == 0)
+            {
+                continue;
+            }
+
+            string destinationPath = Path.Combine(directory, entry.Name);
+            await entry.ExtractToFileAsync(destinationPath, overwrite: true);
+            
+            await using var fileStream = File.OpenRead(destinationPath);
+            byte[] fileHeader = new byte[4];
+            int bytesRead = await fileStream.ReadAsync(fileHeader);
+
+            if (bytesRead >= "MZ"u8.Length && IsExecutable(fileHeader))
+            {
+                executablePath = destinationPath;
+            }
+        }
+
+        await stream.DisposeAsync();
+        return (executablePath != null) ? Path.GetFullPath(executablePath) : null;
+    }
+
+    private static bool IsExecutable(byte[] bytes) => IsWindowsExecutable(bytes) || IsUnixExecutable(bytes);
+
+    private static bool IsWindowsExecutable(byte[] bytes)
+    {
+        return bytes[0] == (byte) 'M'
+               && bytes[1] == (byte) 'Z';
+    }
+
+    private static bool IsUnixExecutable(byte[] bytes)
+    {
+        return bytes[0] == 0x7F
+               && bytes[1] == (byte) 'E'
+               && bytes[2] == (byte) 'L'
+               && bytes[3] == (byte) 'F';
+    }
+}
---
+++ scripts/Service/IInstaller.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Mosic.Scripts.Service;
+
+public interface IInstaller
+{
+    /// <returns>The path to the installed executable, or <c>null</c> if none could be determined.</returns>
+    Task<string> InstallAsync(string path, byte[] bytes);
+}
