146ed3fbec25a6b5eef9d4d0c771a2de7a546d04
Spi3lot <spi3lot@gmail.com>
2025-09-28 02:32:43 +0200

    Add CmdlineUserArgs for easier command line argument parsing and passing

---
+++ scripts/Service/CmdlineUserArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Mosic.Scripts.Service;
+
+public static class CmdlineUserArgs
+{
+    public const string UserArgDelimiter = "++";
+    
+    public const string Replace = "--replace";
+    
+    public static string Set(string key, string value) => $"{key}=\"{value}\"";
+}
